8aead7665cdd805db8953079f13ca75b12f077ea
Ahmed Rezk <150017439+ahmedpve@users.noreply.github.com>
2023-11-18 09:28:05 +0200

    Modify next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", process.env.NEXT_PUBLIC_API_URL],
+    domains: ["localhost", process.env.NEXT_PUBLIC_API_DOMAIN],
   },
 };
 
